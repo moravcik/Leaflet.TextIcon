90dff9bc27f10041f667817222330152955188e1
moravcik <miso.moravcik@gmail.com>
2015-10-08 02:20:14 +0200

    implemented version 0.0.1

---
+++ leaflet-text-icon.js
@@ -0,0 +1,41 @@
+(function () {
+
+	L.TextIcon = L.Icon.extend({
+		options: L.extend({
+			className: 'leaflet-div-icon',
+			getIconUrl: function(color) {
+				//if (L.Browser.retina) - use 2x version
+				return '/images/marker-icon-hole-'+ (color || 'blue') +'.png' 
+			}
+		}, new L.Icon.Default().options),
+
+	 	initialize: function(options) {
+	 		L.extend(options, {
+	 			shadowUrl: L.Icon.Default.imagePath + '/marker-shadow.png'
+	 		});
+	 		L.setOptions(this, options);
+	 		var iconUrl = this.options.getIconUrl(this.options.color);
+	 		this._iconImg = this._createImg(iconUrl);
+	 	},
+		createIcon: function() {
+			var textDiv = document.createElement('div');
+			textDiv.className = 'icon-text';
+			textDiv.innerHTML = this.options.text || '';
+
+			var div = document.createElement('div');
+			div.appendChild(this._iconImg);
+			div.appendChild(textDiv);
+
+			this._setIconStyles(div, 'icon');
+			this._textDiv = textDiv;
+			return div;
+		},
+		setColor: function(color) {
+			this._iconImg.src = this.options.getIconUrl(color);
+		},
+		setText: function(text) {
+			this._textDiv.innerHTML = text || '';
+		}
+	});
+
+}());
